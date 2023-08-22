883076a3822bcc6e3258c630431ebeda8b7780e9
Sahril <103849124+CallMeSahril@users.noreply.github.com>
2023-08-22 22:44:20 +0700

    volume balok

--- app/src/main/java/com/example/belajardicodingpemulaapp/MainActivity.kt
+++ app/src/main/java/com/example/belajardicodingpemulaapp/MainActivity.kt
@@ -5,13 +5,14 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var edtWidth:EditText
     private lateinit var edtHeight:EditText
     private lateinit var edtLength:EditText
     private lateinit var btnCalculate:Button
-    private lateinit var tvResult:EditText
+    private lateinit var tvResult:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,7 +34,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             val inputWidth = edtWidth.text.toString().trim()
             val inputHeight = edtHeight.text.toString().trim()
             val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
-            tvResult.text = volume.toString() dorr
+            tvResult.text = volume.toString()
         }
     }
 }
